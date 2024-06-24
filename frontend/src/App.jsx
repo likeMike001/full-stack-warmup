ee057d85673015ae1ad10c9109256a94785a5121
likeMike001 <like.mike.007.382@gmail.com>
2024-06-24 10:29:07 -0700

    added modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,50 @@
-import { useState , useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import ContactList from './Contact.list'
+import ContactForm from './Contact.form'
 import './App.css'
 
 
 
 function App() {
 
-    const [contacts , setContacts] = useState([])
+  const [contacts, setContacts] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState([false])
 
 
-    useEffect(() => {
-      fetchContacts()
-    } , [])
+  useEffect(() => {
+    fetchContacts()
+  }, [])
 
-    const fetchContacts = async () => {
-      const response = await fetch("http://127.0.0.1:5000/contacts")
-      const data = await response.json()
-      setContacts(data.contacts)
-      console.log(data.contacts)
-    }
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
 
-  return <ContactList contacts={contacts} />
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  return (
+    <>
+      <ContactList contacts={contacts} />
+      <button onClick={openCreateModal}> Create New Contact </button>
+      {setIsModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal} >&times; </span>
+          <ContactForm />
+        </div>
+      </div>
+
+      }
+
+    </>
+  )
 }
 
 export default App
